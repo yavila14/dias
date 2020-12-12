f88c8a0f360e075b9277783efd944b3041fdf5b3
Yeimy <yei14@hotmail.com>
2020-12-11 19:00:11 -0500

    Se agrego .gitignore - dias.dev - maindias.cpp

---
+++ maindias.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+int numero;
+cout <<"Digite un numero :";
+cin>>numero;
+if(numero==1){
+	cout<<"Es dia Lunes ";
+}
+if(numero==2){
+	cout<<"Es dia Martes ";
+}
+if(numero==3){
+	cout<<"Es dia Miercoles ";
+}
+if(numero==4){
+	cout<<"Es dia Jueves ";
+}
+if(numero==5){
+	cout<<"Es dia Viernes ";
+}
+if(numero==6){
+	cout<<"Es dia Sabado ";
+}
+if(numero==7){
+	cout<<"Es dia Domingo ";
+}
+	return 0;
+}
